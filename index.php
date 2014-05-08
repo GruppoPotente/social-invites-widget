4c8d7b865f9943b2d5805b84168b31bff78940c3
Nikola Maric <Nikola.Maric@infobip.com>
2014-05-08 11:00:32 +0200

    Height of iframe holding form increased.

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
       <div class="jumbotron">
         <h1>Infobip Social Invites Demo!</h1>
         <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
-        <p><iframe width="328" height="218" scrolling="no" frameborder="0" src="widget/send-message-form.php"></iframe></p>
+        <p><iframe width="328" height="268" scrolling="no" frameborder="0" src="widget/send-message-form.php"></iframe></p>
       </div>
 
       <!-- Example row of columns -->
